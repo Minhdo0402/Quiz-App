cc6cbff63376402249f4bb42635e775fa6766ad1
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2024-05-23 10:19:53 +0700

    final code

---
+++ server/database/data.js
@@ -0,0 +1,111 @@
+export default [
+    {
+        id: 1,
+        question: 'I have never played badminton before. This is the first time I _____ to play.',
+        options: [
+            'try',
+            'tried',
+            'have tried',
+            'am trying'
+        ]
+    },
+    {
+        id: 2,
+        question: 'People believed that Jane retired because of her poor health.',
+        options: [
+            'Jane is believed to have retired because of her poor health.',
+            'Jane was believed to have retired because of her poor health',
+            'It is believed that Jane retired because of her poor health.',
+            'Jane retired because of her poor health was believed.'
+        ]
+    },
+    {
+        id: 3,
+        question: 'Special Education, specially designed instruction to _______ the unique needs and abilities of disabled or gifted children.',
+        options: [
+            'meet',
+            'see',
+            'have',
+            'find'
+        ]
+    },
+    {
+        id: 4,
+        // từ đồng nghĩa
+        question: 'The teacher gave some `suggestions` on what would come out for the examination.',
+        options: [
+            'symptoms',
+            'demonstrations',
+            'effects',
+            'hints'
+        ]
+    },
+    {
+        id: 5,
+        // từ trái nghĩa
+        question: 'She went to live in another state after the break-up of her `marriage`.',
+        options: [
+            'loneliness',
+            'anniversary',
+            'celebrity',
+            'divorce'
+        ]
+    },
+    {
+        id: 6,
+        // từ cận nghĩa
+        question: 'John is studying hard. He doesn’t want to fail the exam.',
+        options: [
+            'John is studying hard in Oder not to fail the next exam',
+            'John is studying hard in Oder that he not fail the next exam',
+            'John is studying hard so as to fail the next exam',
+            'John is studying hard in Oder to not to fail the next exam'
+        ]
+    },
+    {
+        id: 7,
+        // từ trái nghĩa
+        question: 'Mr. Smith’s new neighbors appear to be very `friendly`.',
+        options: [
+            'amicable',
+            'inapplicable',
+            'hostile',
+            'futile'
+        ]
+    },
+    {
+        id: 8,
+        // Điền vào chỗ trống
+        question: 'programs were often adjusted to meet more general educational needs of the (4) _______.',
+        options: [
+            'people',
+            'country',
+            'community',
+            'population'
+        ]
+    },
+    {
+        id: 9,
+        // Từ đồng nghĩa
+        question: 'When preparing `a diet`, a person should be aware that vitamin D acts to increase the amount of calcium absorbed by the body.',
+        options: [
+            'schedule of exercise ',
+            'medicinal chart',
+            'study of longevity',
+            'nutritional plan'
+        ]
+    },
+    {
+        id: 10,
+        // Từ đồng nghĩa
+        question: 'The director _______ for the meeting by the time I got to his office.',
+        options: [
+            'left ',
+            'had left',
+            'leaves',
+            'will leave'
+        ]
+    },
+];
+
+export const answers = [2, 1, 0, 3, 3, 0, 2, 2, 3, 1];
